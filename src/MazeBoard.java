ddcc7065d7d7ba68f0f7dcfdc2555a709b3c13ec
Ethan Blackburn <eblackb1@slu.edu>
2013-12-12 15:58:30 -0600

    update

--- src/MazeBoard.java
+++ src/MazeBoard.java
@@ -1,30 +1,12 @@
 import java.awt.FlowLayout;
 import java.util.HashSet;
-import java.util.Iterator;
-import java.util.Random;
 import java.util.Set;
-import java.awt.GridLayout;
-import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
-
-import javax.imageio.ImageIO;
-import javax.print.DocFlavor.URL;
-import javax.swing.AbstractAction;
-import javax.swing.Action;
-import javax.swing.ImageIcon;
-import javax.swing.JComponent;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
-import javax.swing.KeyStroke;
 import javax.swing.Timer;
 
 public class MazeBoard extends JPanel{
@@ -132,12 +114,9 @@ public class MazeBoard extends JPanel{
 				pressed.clear();
 			}
 			
-			if((person1.getVertex().getX() == 37) && (person1.getVertex().getY() == 37)){ //if player is on stairs then go to next level
+			if((person1.getVertex().getX() == 37) && (person1.getVertex().getY() == 37) & l.getDifficulty() < 4){ //if player is on stairs then go to next level
 				int diff = l.getDifficulty()+1;
 				remove(l);
-				if(diff > 4){
-					//either make fifth level with trophy or popup window
-				}
 				t.stop();
 				disp.setLevel(diff);
 				l = new Level(diff,width,height);
